ae42286a9fe5f8997849421ae423e9feda93bd84
Idayanis <idayanis.diazfernandez53@stclairconnect.ca>
2016-11-16 12:38:32 -0500

    The super class Places is created as well as the two subclaces Restaurants and Hotles

--- HappyTravels/app/src/main/java/com/idayanisdiazfernandez/happytravels/Hotels.java
+++ HappyTravels/app/src/main/java/com/idayanisdiazfernandez/happytravels/Hotels.java
@@ -4,8 +4,6 @@ package com.idayanisdiazfernandez.happytravels;
  * Created by idasarav on 2016-11-16.
  */
 
-public class Restaurant extends Places {
-
-    String TypeOfFood;
-
+public class Hotels extends Places {
+    
 }
