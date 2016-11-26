ddff65682b6342b0b5abb230da8733524500b945
Maihan Nijat <maihan.nijat84@stclairconnect.ca>
2016-11-26 16:30:11 -0500

    preparing for the merge.

--- HappyTravels/app/src/main/java/com/idayanisdiazfernandez/happytravels/MainActivity.java
+++ HappyTravels/app/src/main/java/com/idayanisdiazfernandez/happytravels/MainActivity.java
@@ -1,12 +1,9 @@
 package com.idayanisdiazfernandez.happytravels;
 
+import android.app.FragmentTransaction;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.view.View;
+import android.app.FragmentManager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -20,7 +17,7 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
         MainFragment.OnFragmentInteractionListener {
 
-    FragmentManager fm = getSupportFragmentManager();
+    FragmentManager fm = getFragmentManager();
     FragmentTransaction ft;
 
     @Override
