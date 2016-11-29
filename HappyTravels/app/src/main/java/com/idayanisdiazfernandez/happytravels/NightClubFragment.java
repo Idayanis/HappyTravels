44674093dfe3b6d1b4703b6d7d679b21d8496f5b
Idayanis <idayanis.diazfernandez53@stclairconnect.ca>
2016-11-29 13:35:53 -0500

    The leyaout for each fragment is updated

--- HappyTravels/app/src/main/java/com/idayanisdiazfernandez/happytravels/NightClubFragment.java
+++ HappyTravels/app/src/main/java/com/idayanisdiazfernandez/happytravels/NightClubFragment.java
@@ -64,7 +64,7 @@ public class NightClubFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_clubs_fragments, container, false);
+        return inflater.inflate(R.layout.fragment_night_club, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
