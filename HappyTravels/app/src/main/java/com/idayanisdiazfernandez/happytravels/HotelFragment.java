e72e51fb87d6c12fe2f4c940e89e475bcab08dec
Idayanis <idayanis.diazfernandez53@stclairconnect.ca>
2016-12-04 20:46:18 -0500

    HotelFragment is updated to set content dynamically based o…  …

--- HappyTravels/app/src/main/java/com/idayanisdiazfernandez/happytravels/HotelFragment.java
+++ HappyTravels/app/src/main/java/com/idayanisdiazfernandez/happytravels/HotelFragment.java
@@ -15,6 +15,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.idayanisdiazfernandez.happytravels.Models.Hotel;
 import com.idayanisdiazfernandez.happytravels.Models.Place;
@@ -33,7 +35,7 @@ import static com.idayanisdiazfernandez.happytravels.Tools.PlacesAdapter.ARG_PLA
  * Use the {@link HotelFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HotelFragment extends Fragment {
+public class HotelFragment extends Fragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -78,109 +80,146 @@ public class HotelFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_hotel, container, false);
+        View view = inflater.inflate(R.layout.fragment_airport, container, false);
 
+        /**
+         * Set content to text boxes.
+         * Set cover image.
+         */
+        ImageView coverPhoto = (ImageView) view.findViewById(R.id.coverPhoto);
+        coverPhoto.setImageResource(mPlace.getThumbnail());
+
+        TextView nameText = (TextView) view.findViewById(R.id.nameText);
+        nameText.setText(mPlace.getName());
+
+        TextView descriptionText = (TextView) view.findViewById(R.id.descriptionText);
+        descriptionText.setText(mPlace.getDescription());
+
+        TextView timingText = (TextView) view.findViewById(R.id.timingText);
+        timingText.setText(mPlace.getTiming());
+
+        TextView priceText = (TextView) view.findViewById(R.id.priceText);
+        priceText.setText(String.valueOf(mPlace.getPrice()));
+
+        TextView addressText = (TextView) view.findViewById(R.id.addressText);
+        addressText.setText(mPlace.getAddress());
+
+        TextView roomText = (TextView) view.findViewById(R.id.roomText);
+        roomText.setText(mPlace.getRoomType());
+
+        TextView swimmingPoolText = (TextView) view.findViewById(R.id.swimmingPoolText);
+        swimmingPoolText.setText(mPlace.getSwimmingPool());
+
+        TextView longTableText = (TextView) view.findViewById(R.id.longTableText);
+        longTableText.setText(mPlace.getLongTable());
+
+        TextView discoText = (TextView) view.findViewById(R.id.discoText);
+        discoText.setText(mPlace.getDisco());
+
+        TextView roomServiceText = (TextView) view.findViewById(R.id.roomServiceText);
+        roomServiceText.setText(mPlace.getRoomService());
+
+        TextView restaurantText = (TextView) view.findViewById(R.id.restaurantText);
+        restaurantText.setText(mPlace.getRestaurant());
+
+
+
+        TextView phoneNumber = (TextView) view.findViewById(R.id.phoneNumber);
+        phoneNumber.setText(mPlace.getPhoneNumber());
+
+        TextView emailAddress = (TextView) view.findViewById(R.id.emailAddress);
+        emailAddress.setText(mPlace.getEmail());
+
+        TextView webPage = (TextView) view.findViewById(R.id.webPage);
+        webPage.setText(mPlace.getWebPage());
+
+        /**
+         *  Find the ImageButtons and set OnClick listener
+         */
         ImageButton emailButton = (ImageButton) view.findViewById(R.id.emailImageButton);
+        emailButton.setOnClickListener(HotelFragment.this);
+
         ImageButton webButton = (ImageButton) view.findViewById(R.id.webImageButton);
+        webButton.setOnClickListener(HotelFragment.this);
+
         ImageButton mapButton = (ImageButton) view.findViewById(R.id.locationImageButton);
+        mapButton.setOnClickListener(HotelFragment.this);
+
         ImageButton shareButton = (ImageButton) view.findViewById(R.id.shareImageButton);
+        shareButton.setOnClickListener(HotelFragment.this);
+
         ImageButton callButton = (ImageButton) view.findViewById(R.id.callImageButton);
-        ImageButton galleryButton = (ImageButton) view.findViewById(R.id.photoImageButton);
+        callButton.setOnClickListener(HotelFragment.this);
 
-        emailButton.setOnClickListener(new View.OnClickListener() {
+        ImageButton photoButton = (ImageButton) view.findViewById(R.id.photoImageButton);
+        photoButton.setOnClickListener(HotelFragment.this);
 
-            @Override
-            public void onClick(View v) {
-                String[]emailAddresses = {"idayanis.diazfernandez53@stclairconnect.ca"};
-                Intent intent = new Intent(Intent.ACTION_SENDTO);
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent intent = null;
+
+        switch (view.getId()) {
+            case R.id.emailImageButton:
+                intent = new Intent(Intent.ACTION_SENDTO);
                 intent.setData(Uri.parse("mailto:"));
-                intent.putExtra(Intent.EXTRA_EMAIL,emailAddresses);
-                if(intent.resolveActivity(getActivity().getPackageManager()) != null){
-                    startActivity(intent);
-                }
-            }
-        });
+                intent.putExtra(Intent.EXTRA_EMAIL, mPlace.getEmail());
+                break;
 
-        webButton.setOnClickListener(new View.OnClickListener() {
+            case R.id.webImageButton:
+                Uri webPage = Uri.parse(mPlace.getWebPage());
+                intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(webPage);
+                break;
 
-            @Override
-            public void onClick(View v) {
-                Uri webpage = Uri.parse("http://www.holguincuba.net/hotels.html");
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(webpage);
-                if(intent.resolveActivity(getActivity().getPackageManager()) != null){
-                    startActivity(intent);
-                }
-            }
-        });
-
-        mapButton.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                Uri geolocation = Uri.parse("geo:0,0?q=@21.1138117,-75.913768,13z(Hotel Location)");
-                Intent intent = new Intent(Intent.ACTION_VIEW);
+            case R.id.locationImageButton:
+                Uri geolocation = Uri.parse("geo:0,0?q=@"+mPlace.getGeoCode());
+                intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(geolocation);
+                break;
 
-                if(intent.resolveActivity(getActivity().getPackageManager()) != null){
-                    startActivity(intent);
+            case R.id.shareImageButton:
+                intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setType("text/plain");
+                intent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Checkout this place!");
+                intent.putExtra(android.content.Intent.EXTRA_TEXT, mPlace.getName() + " " + mPlace.getAddress()
+                        + mPlace.getPhoneNumber() + " " + getActivity().getPackageName());
+                intent.createChooser(intent, "Share via");
+                break;
 
-                }
-                else{
-                    Snackbar snackbar = Snackbar.make(getActivity().findViewById(android.R.id.content),
-                            "No instaled software to complete the task", Snackbar.LENGTH_SHORT);
-                    snackbar.show();
-                }
+            case R.id.callImageButton:
+                intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + mPlace.getPhoneNumber()));
+                break;
 
-            }
-        });
-
-        shareButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(Intent.ACTION_SENDTO);
-                intent.setData(Uri.parse("smsto:"));
-                intent.putExtra("sms_body", " " );
-
-                if(intent.resolveActivity(getActivity().getPackageManager()) != null){
-                    startActivity(intent );
-                }
-                else{
-                    Snackbar snackbar = Snackbar.make(getActivity().findViewById(android.R.id.content),
-                            "No instaled software to complete the task", Snackbar.LENGTH_SHORT);
-                    snackbar.show();
-                }
-
-            }
-        });
-
-        galleryButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            case R.id.photoImageButton:
                 FragmentManager fragmentManager = getFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 fragmentTransaction.setCustomAnimations(R.anim.slide_in, R.anim.slide_out);
                 fragmentTransaction.replace(R.id.mainFragmenLayout, GalleryFragmentPager.newInstance(mPlace))
                         .addToBackStack("tag").commit();
-            }
-        });
+                break;
 
-
-        callButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "Your Phone_number"));
+            default:
+                break;
+        }
+        if (view.getId() != R.id.photoImageButton) {
+            if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
                 startActivity(intent);
-
+            } else {
+                if (view.getId() == R.id.shareImageButton) {
+                    intent.setData(Uri.parse("smsto:"));
+                    startActivity(intent);
+                } else {
+                    Snackbar.make(getActivity().findViewById(android.R.id.content),
+                            "No installed application to complete the task", Snackbar.LENGTH_SHORT).show();
+                }
             }
-        });
-
-
-        return view;
+        }
     }
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
